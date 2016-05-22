957578156fa622985fdb7018abd2ff6858eca461
QuintenDavis <Quinten.R.Davis@gmail.com>
2016-05-22 17:33:52 -0400

    Add files via upload

---
+++ JavaSession3Assignment3.java
@@ -0,0 +1,14 @@
+
+public class JavaSession3Assignment3 {
+
+	public static void main(String[] args) {
+		int N = 4;
+		
+		double randomNumber = Math.random();
+		int randomInt = (int) (N * randomNumber);
+		
+		System.out.println(randomInt);
+
+	}
+
+}
